013d515919b9250d15c9b6c70c0f569e7ae8f8e9
Sergio <s.baldojunior@gmail.com>
2023-10-03 11:45:02 -0300

    Second Commit

--- src/genetic_algorithm/wrapper.py
+++
@@ -1,9 +0,0 @@
-class Wrapper:
-    def __init__(self, sm, df_train, df_test, cwd):
-        self.sm = sm
-        self.df_train = df_train
-        self.df_test = df_test
-        self.cwd = cwd
-
-    def select(self, df_train, df_test, cwd):
-        self.sm.search(self.df_train, self.df_test, self.cwd)
